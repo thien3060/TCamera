3847ffe8b517ed2179757e73884286c049d02f67
thien <thien3060@gmail.com>
2016-05-13 17:59:58 +0700

    fix gallery

--- app/src/main/java/com/user/tcamera/GalleryActivity.java
+++ app/src/main/java/com/user/tcamera/GalleryActivity.java
@@ -1,29 +1,121 @@
 package com.user.tcamera;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.user.tcamera.gallery.GalleryAdapter;
+import com.user.tcamera.gallery.ImageModel;
+import com.user.tcamera.gallery.RecyclerItemClickListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class GalleryActivity extends AppCompatActivity {
+    GalleryAdapter mAdapter;
+    RecyclerView mRecyclerView;
+
+    ArrayList<ImageModel> data = new ArrayList<>();
+    private String[] IMGS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        IMGS = ReadImages().toArray(new String[0]);
+
+        for (int i = 0; i < IMGS.length; i++) {
+
+            ImageModel imageModel = new ImageModel();
+            imageModel.setName("Image " + i);
+            imageModel.setUrl(IMGS[i]);
+            data.add(imageModel);
+
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mAdapter = new GalleryAdapter(GalleryActivity.this, data);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
+                new RecyclerItemClickListener.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        Intent intent = new Intent(GalleryActivity.this, ImageEditorActivity.class);
+                        intent.putExtra("imageUri", data.get(position).getUrl());
+                        startActivity(intent);
+                    }
+                }));
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private List<String> ReadImages() {
+        List<String> s = new ArrayList<String>();
+        String ExternalStorageDirectoryPath = Environment
+                .getExternalStorageDirectory()
+                .getAbsolutePath();
+
+        String targetPath = ExternalStorageDirectoryPath ;
+
+        File targetDirector = new File(targetPath);
+        List<File> allFile = getListFiles(targetDirector);
+        allFile.addAll(getListFiles(getFilesDir()));
+        File[] files = allFile.toArray(new File[0]);
+
+        Arrays.sort( files, new Comparator()
+        {
+            public int compare(Object o1, Object o2) {
+                if (((File)o1).lastModified() > ((File)o2).lastModified()) {
+                    return -1;
+                } else if (((File)o1).lastModified() < ((File)o2).lastModified()) {
+                    return +1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+        for (File file : files){
+            s.add(file.getAbsolutePath());
+        }
+
+        return  s;
+    }
+
+    private List<File> getListFiles(File parentDir) {
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files = parentDir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                inFiles.addAll(getListFiles(file));
+            } else {
+                if(file.getName().endsWith(".jpg")){
+                    inFiles.add(file);
+                }
+            }
+        }
+        return inFiles;
+    }
 }
--- app/src/main/java/com/user/tcamera/MainActivity.java
+++ app/src/main/java/com/user/tcamera/MainActivity.java
@@ -1,23 +1,12 @@
 package com.user.tcamera;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final int ACTIVITY_START_CAMERA_APP = 0;
@@ -29,15 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
